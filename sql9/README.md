cbf9eed0e940463ca91beac753d6bb7c96734350
yusuf <68552206+yusufkukus58@users.noreply.github.com>
2022-08-30 21:23:59 +0300

    Add files via upload

---
+++ sql9/README.md
@@ -0,0 +1,16 @@
+﻿# PatikaDev
+
+## SQLodev9
+
+### ans1
+![ans1](1.png)
+
+
+### ans2
+![ans2](2.png)
+
+### ans3
+![ans3](3.png)
+
+
+
---
+++ sql9/innerJoin.sql
@@ -0,0 +1,19 @@
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini 
+-- birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT country,city FROM country
+INNER JOIN city ON country.country_id = city.country_id;
+
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
+-- first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT first_name,last_name,payment_id FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name 
+-- isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT first_name,last_name,rental_id FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id;
